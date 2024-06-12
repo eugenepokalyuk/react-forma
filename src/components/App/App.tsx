2f7e600c11a594107aabd4ef942484646371e354
Evgenii <eugene.pokalyuk@gmail.com>
2024-06-12 16:25:38 +0700

    Replce routings on App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import ErrorPage from '../../pages/ErrorPage';
+import HomePage from '../../pages/HomePage';
+import ProfilePage from '../../pages/ProfilePage';
+import { DEFAULT_PATH, ERROR_PATH, PROFILE_PATH } from '../../utils/consts';
 import NavigationMenu from '../NavigationMenu/NavigationMenu';
 
 const App = () => {
   return (
-    <div className='dark:bg-[#282A22] bg-[#EDFBEB] pb-24 flex justify-center'>
-      <div className='w-full md:max-w-xs'>
-        <Outlet />
-        <NavigationMenu />
+    <Router>
+      <div className='dark:bg-[#282A22] bg-[#EDFBEB] flex justify-center min-h-screen pb-24'>
+        <div className='w-full md:max-w-xs'>
+          <Routes>
+            <Route path={DEFAULT_PATH} element={<Outlet />}>
+              <Route index element={<HomePage />} />
+              <Route path={PROFILE_PATH} element={<ProfilePage />} />
+              <Route path={ERROR_PATH} element={<ErrorPage />} />
+            </Route>
+          </Routes>
+          <NavigationMenu />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
