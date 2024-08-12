19214b050efbc6f417db6c4fb69874edacfeacc6
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 17:35:25 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
-import App from './components/App/App';
-import { ThemeProvider } from './context/ThemeContext';
+
 import './index.css';
+import App from './components/App/App';
 import store from './store';
+import { ThemeProvider } from './context/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
