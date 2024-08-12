9c378fd6f340f709a0f639dfa7b2b233537d4f19
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 17:44:17 +0700

    Update ErrorPage.tsx

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -4,6 +4,7 @@ const ErrorPage = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen text-white space-y-6">
             <p className="text-2xl dark:text-[#FFFFFF] text-[#444444]">Страница не найдена 😔</p>
+            
             <Link to="/" className="bg-[#BBFF00] text-[#444444] py-[9px] px-[21px] rounded-lg w-auto text-xs font-normal leading-[14px]">Вернуться на главную</Link>
         </div>
     );
