0e0a2a7165509192167dd8dfbd8332fdc58fec42
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 17:48:07 +0700

    Update consts.ts

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,12 +1,20 @@
 export const THEME: string = 'theme';
+
 export const DARK_THEME: string = 'dark';
+
 export const DAEFAUL_THEME: string = 'default';
 
 // routes
 export const DEFAULT_PATH = "/";
+
 export const PROFILE_PATH = "/profile";
+
 export const PROGRAMS_PATH = "/programs";
+
 export const PROGRAM_PATH = "/programs/:programId";
+
 export const ACHIEVEMENTS_PATH = "/achievements";
+
 export const SUBSCRIPTION_PATH = "/subscription";
+
 export const ERROR_PATH = "*";
