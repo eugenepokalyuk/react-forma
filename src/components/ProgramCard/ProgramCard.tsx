0162dc059a3983717f5faf32c73fc5d368f03783
Evgenii <111701135+eugenepokalyuk@users.noreply.github.com>
2024-08-12 17:40:08 +0700

    Update ProgramCard.tsx

--- src/components/ProgramCard/ProgramCard.tsx
+++ src/components/ProgramCard/ProgramCard.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import Button from '../UI/Button/Button';
 import Tag from '../UI/Tag/Tag';
 
@@ -11,7 +12,7 @@ interface ProgramCardProps {
     isFree: boolean;
 }
 
-const ProgramCard: React.FC<ProgramCardProps> = ({ id, title, description, isLocked, isFree }) => {
+const ProgramCard: FC<ProgramCardProps> = ({ id, title, description, isLocked, isFree }) => {
     const navigate = useNavigate();
 
     const handleStart = () => {
@@ -25,10 +26,14 @@ const ProgramCard: React.FC<ProgramCardProps> = ({ id, title, description, isLoc
         <div className={`bg-[#111111]/[.16] p-3 space-y-2 rounded-xl ${isLocked ? 'opacity-50 cursor-not-allowed' : ''}`}>
             <div className="flex flex-row justify-between items-start">
                 <h3 className='text-lg leading-6 font-medium dark:text-[#FFFFFF] text-[#444444]'>{title}</h3>
+                
                 {isLocked && <Tag variant='red' font='small' className='font-semibold'>Заблокировано</Tag>}
+                
                 {isFree && <Tag variant='green' font='small' className='font-semibold'>Бесплатно</Tag>}
             </div>
+            
             <p className='text-sm leading-[18px] font-normal dark:text-[#FFFFFF]/[.56] text-[#444444]'>{description}</p>
+            
             <div>
                 <Button variant='gray' className='w-full' disabled={isLocked} onClick={handleStart}>Начать</Button>
             </div>
